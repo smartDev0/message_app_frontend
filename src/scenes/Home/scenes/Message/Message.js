3d8f82860ff969affb607b5f15aad5be306f3484
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-10-13 22:27:28 +0800

    add crud functioin

--- src/scenes/Home/scenes/Message/Message.js
+++ src/scenes/Home/scenes/Message/Message.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './style.css';
 import { HiOutlinePlus } from 'react-icons/hi';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import { FaRegEdit } from 'react-icons/fa';
+import { AiOutlineDelete } from 'react-icons/ai';
 import { Modal, Form, Button } from "react-bootstrap";
 import fake_user from "../../../../assets/imgs/avatar-circle-tale.svg";
 import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
 
 const MODAL_STYLES = {
     overlay: {
@@ -42,10 +45,12 @@ class Message extends React.Component {
             value: null,
             messages: {},
             record: [
-                {
-                    date: 'Thu Oct 08 2020 00:00:00 GMT+0800 (Taipei Standard Time)',
-                    message:'aaa'}
-            ]
+                // {
+                //     date: 'Thu Oct 08 2020 00:00:00 GMT+0800 (Taipei Standard Time)',
+                //     message:'aaa'}
+            ],
+            currentRecord: [],
+            id:''
         }
     }
     handleClose() {
@@ -67,8 +72,59 @@ class Message extends React.Component {
         this.setState({ createModalActive: false, value: null, message:''})
 
     }
+    handleUpdateSubmit = () => {
+        const data = {
+            date: this.state.value,
+            message: this.state.message
+        };
+        var records = this.state.record
+        records.map((item, index) => {
+            if (index == this.state.id) {
+                records[index].date = this.state.value;
+                records[index].message = this.state.message;
+            }
+        });
+        this.setState({record: records})
+        this.setState({ editModalActive: false, value: null, message: '' })
+    }
+    onChangeEdit = (i) => {
+        this.setState({id:i})
+        var remainRecord = this.state.record.map((item, index) => {
+            if (index == i) {
+                return item;
+            }
+        });
+        var filterData = remainRecord.filter(item => {
+            if (item !== undefined) {
+                return item
+            }
+        });
+        this.setState({ currentRecord: filterData, value:filterData[0].date, message:filterData[0].message });
+
+        this.setState({
+            editModalActive: true
+        })
+    }
+    onChangeDelete = (i) => {
+        var remainRecord = this.state.record.map((item, index) => {
+            if (index !== i) {
+                return item;
+            }
+        });
+        var filterData = remainRecord.filter(item => {
+            if (item !== undefined) {
+                return item
+            }
+        });
+        this.setState({record: filterData})
+    }
+    handleEditClose = () => {
+        this.setState({
+            editModalActive: false
+        })
+    }
     render() {
-        console.log(this.state.record)
+        const { record } = this.state;
         return (
             <>
                 <nav className="top-navbar navbar-expand-lg navbar-light">
@@ -84,11 +140,34 @@ class Message extends React.Component {
                         </div>
                     </div>
                 </nav>
-                <div className="message-content d-v-center">
+                {record.length > 0 ?
+                    (<div className="message-content">
+                        {record.map((item, i) => (
+                            <div className="message-item" key={i}>
+                                <div className="">
+                                    <div className="row">
+                                        <div className="col-md-10 col-9">
+                                            <div className="message-title">
+                                                {item.message}
+                                            </div>
+                                            <div className="message-date">
+                                                {moment(item.date).format("DD/MM/YYYY-hh:mm:ss")}
+                                            </div>
+                                        </div>
+                                        <div className="col-md-2 col-3 d-v-center">
+                                            <FaRegEdit style={{ fontSize: 22, cursor: "pointer" }} onClick={() => this.onChangeEdit(i)}/>
+                                            <AiOutlineDelete style={{ fontSize: 25, cursor: "pointer" }} onClick={() => this.onChangeDelete(i)}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    
+                </div>) : (<div className="message-content d-v-center">
                     <h4 className="empty-text">
                         Empty list.Add new message...
                     </h4>
-                </div>
+                </div>)}
                 <div className="add-message d-v-center" onClick={() => this.setState({createModalActive:true})}>
                     <HiOutlinePlus style={{ fontSize: 26,color:"white"}}/>
                 </div>
@@ -120,6 +199,37 @@ class Message extends React.Component {
                         />
                         <Button variant="primary" className={"confirm-button"} onClick={() => { this.handleSubmit() }}>
                             Save
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                {/* edit modal */}
+                <Modal
+                    show={this.state.editModalActive}
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    style={MODAL_STYLES}
+                    onHide={() => { this.handleEditClose() }}>
+                    <Modal.Header>
+                        <Modal.Title>Edit Message</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="row">
+                            <div className="col-md-2 col-2">
+                                <img src={fake_user} alt="" className={"avatar"} />
+                            </div>
+                            <div className="col-md-10 col-10">
+                                <Form.Control as="textarea" placeholder="Message..." rows="3" onChange={(e) => { this.setState({ message: e.target.value }) }} value={this.state.message ? this.state.message : ''} />
+                            </div>
+                        </div>
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <DateTimePicker
+                            onChange={this.handleChange}
+                            value={this.state.value}
+                        />
+                        <Button variant="primary" className={"confirm-button"} onClick={() => { this.handleUpdateSubmit() }}>
+                            Update
                         </Button>
                     </Modal.Footer>
                 </Modal>
